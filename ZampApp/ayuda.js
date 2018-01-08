ee01e2b8973ac8e3457852c7b6eb121df5094357
Unknown <olga.camsa@gmail.com>
2018-01-08 18:06:16 +0100

    AJAX AJUDA

--- ZampApp/ayuda.js
+++ ZampApp/ayuda.js
@@ -1,9 +1,43 @@
 
 document.getElementById("submit").onclick = feedback;
 
-function feedback() {
-    alert("¡Muchas gracias, contestaremos a tu mensaje lo antes posible!");
-    
+function feedback(evt) {
+    evt.preventDefault();
+
+    let request = new XMLHttpRequest();
+    let url = "http://www.mocky.io/v2/5a538fd43000006f2c1ebfe0x"
+    pregunta.nombre = document.getElementById("nombre").value;
+    pregunta.email = document.getElementById("email").value;
+    pregunta.mensaje = document.getElementById("mensaje").value;
+
+    // console.log(pregunta.nombre , pregunta.email , pregunta.mensaje);
+
+    if(pregunta.nombre && pregunta.email && pregunta.mensaje){
+        request.open("POST", url);
+        request.onload = function () {
+            if(request.status==200){
+                alert("¡Muchas gracias, contestaremos a tu mensaje lo antes posible!");
+                console.log('request:', request)
+            }else{
+                console.log('Error:', request)
+            }
+        };
+        request.send("nomre=xxx&email=xxx&mensaje=xxx");
+    }else{
+        alert("Completa todos los campos para poder enviar tu mensaje");
+    }
 }
+
+
+
+class pregunta {
+    constructor() {
+        this.nombre = "";
+        this.email = "";
+        this.mensaje = "";
+    }
+}
+
+
 
 
