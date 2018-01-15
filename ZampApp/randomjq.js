7db8da1c7dc87ced66d1a8f04e7b9ecaa4f3a800
Unknown <marcosalbares@gmail.com>
2018-01-15 15:13:18 +0100

    Mejora random y footer

--- ZampApp/randomjq.js
+++ ZampApp/randomjq.js
@@ -10,14 +10,22 @@ function RecojeDatos() {
 
 // Envio al server
 var url = 'http://www.mocky.io/v2/5a5893682d0000781ad2e57b';
+var badurl = 'http://www.mocky.io/v2/5a5cabf22e00005e199f83bd';
 function EnvioDatos(data) {
     $.post(url, data,
         function (data, textStatus, jqXHR) {
             console.log('Recibido');
+            $('#textModal').html("Me gusta tu elección amigo<br>ZamsApp's Actitude Always");
+            $('#myModal').modal('show');
+            setTimeout(enlaceresultados, 2000);
         },
     )
     .fail(function (data, textStatus, jqXHR) {
             console.log('No Recibido');
+            // RecojeDatos();
+            $('#textModal').html("Parece que tenemos algún tipo de problema en la cocina<br>En unos segundos te redirigiremos a la página principal");
+            $('#myModal').modal('show');
+            setTimeout(enlacehome, 6000);
         },
     )
 }
@@ -31,16 +39,19 @@ $('#myModal').modal({
 });
 
 // enlace a pagina resultados
-function enlace() {
-    window.location.href = "http://localhost:8000/dexterminadores/Dexterminadores/ZampApp/resultados.html";
+function enlaceresultados() {
+    window.location.href = "resultados.html";
+}
+
+// enlace a pagina home
+function enlacehome() {
+    window.location.href = "home.html";
 }
 
 // Accion del boton
 $('#btnAdelante').click(function (evento) {
     evento.preventDefault();
-    $('#myModal').modal('show');
     RecojeDatos();
     EnvioDatos(arrRandom);
-    setTimeout(enlace, 2000);
 })
 
