f047c16d0a47287d6d74e1e863a0fb9aac051a32
Angel <yhosy_soldier@hotmail.com>
2018-01-15 12:51:05 +0100

    Javascript con explicaciones

--- ZampApp/jsHome.js
+++ ZampApp/jsHome.js
@@ -1,12 +1,16 @@
+// --Función donde se guardará los datos de los ingredientes--
 function Ingredientes() {
     nombre = '';
     seleccionado = false;
 }
 
+
+// --Funcion que se activa al pulsar el botón de buscar en la parte de la derecha
 $('#btnBusqueda').click(function (evnt) {
-    // evnt.preventDefault();
+    // --Variable que se enviara a la base de datos--
     var platos = [];
 
+    // --recoge el nombre y si esta checkeado de cada ingrediente/input--
     $('input').each(function () {
         let x = new Ingredientes
         x.nombre = this.attributes[0].value;
@@ -14,6 +18,8 @@ $('#btnBusqueda').click(function (evnt) {
         platos.push(x);
     })
 
+
+    // Envia la variable que contiene los nombres y checkeos a la base de datos--
     $.ajax({
         type: "POST",
         url: "http://www.mocky.io/v2/5a5c70c92e0000050a9f82a1",
