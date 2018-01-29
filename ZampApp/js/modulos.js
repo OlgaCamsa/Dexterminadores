3e4288242437aa01e2f9116f69720aa38a3c9280
Angel <yhosy_soldier@hotmail.com>
2018-01-29 14:44:06 +0100

    Modulo filtro y jsHome.js

--- ZampApp/js/modulos.js
+++ ZampApp/js/modulos.js
@@ -1,19 +1,33 @@
-// Modulo filtro
-var valores = {};
-var listaFiltrada = [];
 
-function datas() {
+var LSModule = (function () {
+    var valores = {};
+
+    // ---Función que rellena el objeto valores  y lo devuelve repleto de los inputs de la página---
+    function _preparaDatos() {
+        valores = {};
         $('select').each(function () {
             valores[this.name] = this.value;
-        })
+        });
 
         $('input:checked[type=checkbox]').each(function () {
             valores[this.name] = (valores[this.name] ? valores[this.name] + ',' : '') + this.value;
-        })
+        });
 
         $('input').not('input[type=checkbox]').each(function () {
             valores[this.name] = (valores[this.name] ? valores[this.name] + ',' : '') + this.value;
-        })
+        });
         return valores;
-    console.log(valores);
     }
+
+    // ---Función para guardar los datos de cada input en el local storage con la clave inputs---
+    function _guardarDatos() {
+
+        window.localStorage.setItem('inputs',JSON.stringify(_preparaDatos()));
+    }
+
+    // ---Funciones disponibles al llamar LSModules---
+    return {
+        GuardarInputs: _guardarDatos,
+        LeerInputs: _preparaDatos
+    }
+})();
--- ZampApp/jsHome.js
+++ ZampApp/jsHome.js
@@ -1,35 +1,5 @@
-// --Función donde se guardará los datos de los ingredientes--
-function Ingredientes() {
-    nombre = '';
-    seleccionado = false;
-}
-
-
-// --Funcion que se activa al pulsar el botón de buscar en la parte de la derecha
+//--- al hacer click en el boton busqueda llama al modulo LSModule para guardar los datos de los inputs---
 $('#btnBusqueda').click(function (evnt) {
-    // --Variable que se enviara a la base de datos--
-    var platos = [];
-
-    // --recoge el nombre y si esta checkeado de cada ingrediente/input--
-    $('input').each(function () {
-        let x = new Ingredientes
-        x.nombre = this.attributes[0].value;
-        x.seleccionado = this.checked;
-        platos.push(x);
-    })
-
-
-    // Envia la variable que contiene los nombres y checkeos a la base de datos--
-    $.ajax({
-        type: "POST",
-        url: "http://www.mocky.io/v2/5a5c70c92e0000050a9f82a1",
-        data: platos,
-        success: function (response) {
-            console.log('Conseguido',platos);
-        }
-    })
-    .fail(function(parametros){
-        console.log('Epic Fail')
-        
-    });
-})
+    // event.preventDefault();
+   LSModule.GuardarInputs();
+});
