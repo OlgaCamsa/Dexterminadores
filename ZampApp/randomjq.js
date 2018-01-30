a4d61c83827efc4741e1d99783e919743d585a2c
Unknown <marcosalbares@gmail.com>
2018-01-30 17:26:45 +0100

    mejora random con mustache y modulo

--- ZampApp/randomjq.js
+++ ZampApp/randomjq.js
@@ -1,26 +1,27 @@
-// Recojer datos
-var arrRandom = {};
-function RecojeDatos() {
-    arrRandom = {};
-    $('form input:checked').each(function () {
-        arrRandom[this.id] = (arrRandom[this.id] ? arrRandom[this.id] + ',' : '') + this.value;
-    });
-    console.log(arrRandom);
-}
+// // Recojer datos sin modulo
+// var arrRandom = {};
+// function RecojeDatos() {
+//     arrRandom = {};
+//     $('form input:checked').each(function () {
+//         arrRandom[this.id] = (arrRandom[this.id] ? arrRandom[this.id] + ',' : '') + this.value;
+//     });
+//     console.log(arrRandom);
+// }
 
-// Envio al server
+// Envio al server 
 var url = 'http://www.mocky.io/v2/5a5893682d0000781ad2e57b';
 var badurl = 'http://www.mocky.io/v2/5a5cabf22e00005e199f83bd';
-function EnvioDatos(data) {
-    $.get(url, data,
+function EnvioDatos(obj) {
+    $.post(url, obj,
         function (data, textStatus, jqXHR) {
             console.log('Recibido');
+            console.log(obj);
             $('#textModal').html("Me gusta tu elección amigo<br>ZampApp's Actitude Always");
             $('#myModal').modal('show');
             setTimeout(enlaceresultados, 6000);
         },
     )
-    .fail(function (data, textStatus, jqXHR) {
+    .fail(function (obj, textStatus, jqXHR) {
             console.log('No Recibido');
             // RecojeDatos();
             $('#textModal').html("Parece que tenemos algún tipo de problema en la cocina<br>En unos segundos te redirigiremos a la página principal");
@@ -51,7 +52,6 @@ function enlacehome() {
 // Accion del boton
 $('#btnAdelante').click(function (evento) {
     evento.preventDefault();
-    RecojeDatos();
-    EnvioDatos(arrRandom);
+    LSModule.GuardarInputs();
+    EnvioDatos(LSModule.LeerInputs());
 })
-
---
+++ ZampApp/randomplantilla.js
@@ -0,0 +1,48 @@
+var preguntas = {
+    preg: [
+        {
+            posicion: "topizq",
+            titulo: "Cantidad",
+            descripcion: "Tenemos comidas para compartir",
+            opciones: [
+                { type: "checkbox", name: "cantidad", value: "individual", text: "Individual" },
+                { type: "checkbox", name: "cantidad", value: "compartir", text: "Para compartir" }],
+        },
+        {
+            posicion: "topder",
+            titulo: "Temperatura",
+            descripcion: "¿Prefieres una plato para entrar en calor o fresco?",
+            opciones: [
+                { type: "checkbox", name: "temp", value: "caliente", text: "Caliente" },
+                { type: "checkbox", name: "temp", value: "frio", text: "Frío" }],
+        },
+        {
+            posicion: "downizq",
+            titulo: "Alergias",
+            descripcion: "¿Tienes alguna intolerancia alimenticia?",
+            opciones: [
+                { type: "checkbox", name: "alergias", value: "gluten", text: "Gluten" },
+                { type: "checkbox", name: "alergias", value: "lactosa", text: "Lactosa" },
+                { type: "checkbox", name: "alergias", value: "frutossecos", text: "Frutos secos" }],
+        },
+        {
+            posicion: "downder",
+            titulo: "Sabor",
+            descripcion: "¿Te gusta el picante o eres mas de agridulce?",
+            opciones: [
+                { type: "checkbox", name: "pic", value: "picante", text: "Picante" },
+                { type: "checkbox", name: "pic", value: "nadapicante", text: "Nada picante" },
+                { type: "checkbox", name: "pic", value: "agridulce", text: "Agridulce" }],
+        }
+    ]
+};
+console.log(preguntas);
+var tpl = "{{#preg}}<div id='{{posicion}}'>{{>h3}}</div>{{/preg}}";
+var varh3 = {
+    h3: "<div class='pregunta'><h3>{{titulo}}</h3></div>" +
+        "<div><h5>{{descripcion}}</h5></div>" +
+        "<div><form action=''>{{#opciones}}<input type='{{type}}' name='{{name}}' value='{{value}}' id='{{value}}'>" + "<label for='{{value}}'>{{text}}</label>" + "<br>{{/opciones}}</form></div>"
+};
+
+var html = Mustache.to_html(tpl, preguntas, varh3);
+$('#asks').html(html);
