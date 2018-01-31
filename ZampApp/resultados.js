51ab5d4febb9de44b44f51a9a1cd4c3fe7a12094
Unknown <vera.dyafu@gmail.com>
2018-01-31 12:52:32 +0100

    commit res

--- ZampApp/resultados.js
+++ ZampApp/resultados.js
@@ -20,83 +20,107 @@ $("#ex2").slider({
 
 /*FILTROS*/
 
+window.onload = function(){
+	LSModule.CargarInputs;
+	var valores = JSON.parse(window.localStorage.getItem('inputs'));
+	console.log("valores:", valores);
 
-var myArr;
-var requestFiltro = new XMLHttpRequest();
-var url = "./listaDeProductos.json";
-requestFiltro.onload = function() {
-	if (this.readyState == 4) {
-		if (this.status == 200) {
-			console.log('Exito');
-			myArr = JSON.parse(this.responseText);
-			window.onload = function(){
-				LSModule.CargarInputs;
-				var valores = JSON.parse(window.localStorage.getItem('inputs'));
-				console.log(valores);
+	var myArr;
+	var requestFiltro = new XMLHttpRequest();
+	var url = "./listaDeProductos.json";
+	requestFiltro.onload = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				console.log('Exito');
+				myArr = JSON.parse(this.responseText);
 				CrearListaFiltrada(valores, myArr);
+
+				//ShowLista(myArr);
 			}
-			
-			if ($('#botonFiltro').click(function(e){
-				e.preventDefault();
-				console.log("true");
-				var valores={};
-				valores=LSModule.LeerInputs();
-				CrearListaFiltrada(valores, myArr);
-			}));
-			
-			//ShowLista(myArr);
-			
+			else{
+				alert('Un error');
+			}
 		}
-		else{
-			alert('Un error');
+		else {
+			alert('La página se está cargando');
 		}
-	}
-	else {
-		alert('La página se está cargando');
-	}
-};
-requestFiltro.open("POST", url, true);
-requestFiltro.send();
+	};
+	requestFiltro.open("POST", url, true);
+	requestFiltro.send();
+
+	$('#botonFiltro').click(function(e){
+		localStorage.removeItem("inputs");
+		valores = {};
+		console.log("true");
+		valores=LSModule.LeerInputs();
+		console.log('valores2:', valores);
+		CrearListaFiltrada(valores, myArr);
+	});
+}
+
 
 
 var listaFiltrada = [];
 
 function CrearListaFiltrada(valores, myArr){
+	var prefer = ["categoria", "cantidad", "preferencia", "pic", "temp"];
+
+	for(let k=0;k<prefer.length;k++){
+		if(valores[prefer[k]] !== null){
+		console.log("keys", Object.keys(valores));
+		console.log('prefer is not null');
+			if(valores[prefer[k]].split(",").length==1){
+				listaFiltrada = listaFiltrada.concat(myArr.filter(plato => {
+					return plato[prefer[k]] == valores[prefer[k]]
+				}));
+			}
+			else{
+				for(let i=0; i<valores[prefer[k]].split(",").length;i++){
+					listaFiltrada = listaFiltrada.concat(myArr.filter(plato => {
+						return plato[prefer[k]] == valores[prefer[k]].split(",")[i]
+					
+					}));
+				}
+			}
+		}
+	}
 	
-	if (valores.categoria !== "undefined"){
-		console.log("jjfklj");
-		listaFiltrada = listaFiltrada.concat(myArr.filter(plato => plato.categoria == valores.categoria));
-	}
-	else if (valores.cantidad !== "undefined"){
-		listaFiltrada =listaFiltrada.concat(myArr.filter(plato => plato.cantidad == valores.cantidad));
-	}
-	else if(valores.preferencia !== "undefined"){
-		console.log("sdf");
-		listaFiltrada =listaFiltrada.concat(myArr.filter(plato => plato.preferencia == valores.preferencia));
-	}
-	valores = {};
+
 	var myset = new Set(listaFiltrada);
-	console.log("4", myset);
 	listaFiltrada = Array.from(myset);
-	console.log("5", listaFiltrada);
 
 	pintarPlatos(listaFiltrada);
 	
-	
 
-
-	/*if (valores.categoria !== "undefined"){
-		console.log("jjfklj");
-		listaFiltrada = listaFiltrada.concat(myArr.filter(plato => plato.categoria == valores.categoria));
+	/*if (valores.categoria !== null){
+		console.log("keys", Object.keys(valores));
+		console.log('categoria is not null');
+		if(valores.categoria.split(",").length==1){
+			listaFiltrada = listaFiltrada.concat(myArr.filter(plato => plato.categoria == valores.categoria));
+		}
+		else{
+			for(let i=0; i<valores.categoria.split(",").length;i++){
+				listaFiltrada = listaFiltrada.concat(myArr.filter(plato => plato.categoria == valores.categoria.split(",")[i]));
+			}
+		}
 	}
-	else if (valores.cantidad !== "undefined"){
+	else if (valores.cantidad !== null){
+		console.log('cantidad is not null');
 		listaFiltrada =listaFiltrada.concat(myArr.filter(plato => plato.cantidad == valores.cantidad));
 	}
-	else if(valores.preferencia !== "undefined"){
-		console.log("sdf");
+	else if(valores.preferencia !== null){
+		console.log('preferencia is not null');
 		listaFiltrada =listaFiltrada.concat(myArr.filter(plato => plato.preferencia == valores.preferencia));
-	}	*/				
-	
+	}
+	else if(valores.pic !== null){
+		console.log('pic is not null');
+		listaFiltrada =listaFiltrada.concat(myArr.filter(plato => plato.pic == valores.pic));
+	}
+	else if(valores.temp !== null){
+		console.log('temp is not null');
+		listaFiltrada =listaFiltrada.concat(myArr.filter(plato => plato.temp == valores.temp));
+	}*/
+
 	
 	
 	
@@ -147,18 +171,3 @@ function pintarPlatos(listaFiltrada){
 }
 listaFiltrada.splice(0,listaFiltrada.length);
 
-
-/*FIN FILTROS*/
-
-/*var listaFiltrada = [];
-	for(i=0; i<myArr.length;i++){
-		if (valores.cantidad !== null){
-			
-		}
-		if ((myArr[i].cantidad == valores.cantidad)&&
-			(myArr[i].categoria == valores.categoria)&&
-			(myArr[i].preferencia == valores.preferencia)){
-			listaFiltrada.push(myArr[i]);
-		}						
-	};
-	pintarPlatos(listaFiltrada);*/
