a868df708df57d01eae0e0edcdd7ea347f2c87bf
Angel <yhosy_soldier@hotmail.com>
2018-01-31 15:50:28 +0100

    Spec Actualizados

--- ZampApp/Librerias/Jasmine/spec/AyudaSpec.js
+++ ZampApp/Librerias/Jasmine/spec/AyudaSpec.js
@@ -1,28 +1,35 @@
-
-
-describe("recogida de inputs", function () {
-    var pregunta;
-
-    pregunta = {
-        name: "olga",
-        email: "olga@gmail.com",
-        mensaje: "Ayuda",
-    }
-    it("get the email", function getinfo() {
-        expect(pregunta).toEqual(jasmine.objectContaining({
-            name: "olga"
-        }));
+describe('Contact',function(){
+  
+    it('Asegurar que la función contact devuelve una función',function(){
+        expect(Contact).toEqual(jasmine.any(Function))
     });
 
-    it("get the email", function getinfo() {
-        expect(pregunta).toEqual(jasmine.objectContaining({
-            email: "olga@gmail.com"
-        }));
-    });
+})
 
-    it("get the message", function getinfo() {
-        expect(pregunta).toEqual(jasmine.objectContaining({
-            mensaje: "Ayuda"
-        }));
-    });
-});
+// describe("recogida de inputs", function () {
+//     var pregunta;
+
+//     pregunta = {
+//         name: "olga",
+//         email: "olga@gmail.com",
+//         mensaje: "Ayuda",
+//     }
+//     it("get the email", function getinfo() {
+//         expect(pregunta).toEqual(jasmine.objectContaining({
+//             name: "olga"
+//         }));
+//     });
+
+//     it("get the email", function getinfo() {
+//         expect(pregunta).toEqual(jasmine.objectContaining({
+//             email: "olga@gmail.com"
+//         }));
+//     });
+
+//     it("get the message", function getinfo() {
+//         expect(pregunta).toEqual(jasmine.objectContaining({
+//             mensaje: "Ayuda"
+//         }));
+//     });
+// });
+
